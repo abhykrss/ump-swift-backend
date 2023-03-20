1f16f524b1a7c7ff14caa4a34f2f42040e962aae
Abhay Tiwari <abhay90458@gmail.com>
2023-03-20 09:43:48 +0530

    error handling

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,11 @@
 "use strict";
+//Configuring ENVIRONMENT variables
+// import dotenv from "dotenv";
+// dotenv.config();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-//Configuring ENVIRONMENT variables
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
 //Crucial Imports -->
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
