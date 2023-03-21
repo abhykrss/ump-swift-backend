a99569b88f5c22e3a475e48262c73cd247fc56fd
Abhay Tiwari <abhay90458@gmail.com>
2023-03-22 01:46:03 +0530

    cors

--- index.ts
+++ index.ts
@@ -18,14 +18,14 @@ import {
 //Router Config -->
 const app: Express = express();
 const port = process.env.PORT;
-
+const frontEndUrl =
+  "https://641a0ee70c8e79064a869ea5--stellular-maamoul-1bdee3.netlify.app";
 //MiddleWares -->
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin:
-      "https://641a0ee70c8e79064a869ea5--stellular-maamoul-1bdee3.netlify.app",
+    origin: frontEndUrl,
   })
 );
 //Routes -->
@@ -43,4 +43,5 @@ app.put("/updateAttendance", updateAttendance);
 
 app.listen(port, () => {
   console.log(`Server listening to Port ${port}`);
+  console.log(`Front-End hosted on ${frontEndUrl}`);
 });
